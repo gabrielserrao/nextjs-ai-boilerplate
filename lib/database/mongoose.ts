c2a391ac4e7b5d63ca0584ac8b8eecf740e57f0a
gabrielserrao <39321291+gabrielserrao@users.noreply.github.com>
2024-03-04 19:56:27 +0100

    fix

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -1,31 +1,33 @@
-import mongoose , {Mongoose} from "mongoose";
 
-const MONGODB_URL = process.env.MONGO_URL;
+import mongoose, { Mongoose } from 'mongoose';
+
+const MONGODB_URL = process.env.MONGODB_URL;
 
 interface MongooseConnection {
-    conn: Mongoose | null;
-    promise: Promise<Mongoose> | null;
+  conn: Mongoose | null;
+  promise: Promise<Mongoose> | null;
 }
 
-let cached: MongooseConnection = (global as any).mongoose || {};
+let cached: MongooseConnection = (global as any).mongoose
 
-if (!cached) {
-    cached = (global as any).mongoose = 
-    { conn: null, promise: null };
+if(!cached) {
+  cached = (global as any).mongoose = { 
+    conn: null, promise: null 
+  }
 }
 
 export const connectToDatabase = async () => {
-    if (cached.conn) {
-        return cached.conn;
-    }
+  if(cached.conn) return cached.conn;
 
-    if(!MONGODB_URL) throw new Error("MONGO_URL is not defined");
+  if(!MONGODB_URL) throw new Error('Missing MONGODB_URL');
 
-    cached.promise = cached.promise || mongoose.connect(MONGODB_URL, 
-        {dbName:"Cluster0" , bufferCommands: false })
+  cached.promise = 
+    cached.promise || 
+    mongoose.connect(MONGODB_URL, { 
+      dbName: 'Cluster0', bufferCommands: false 
+    })
 
-    cached.conn = await cached.promise;
-    return cached.conn;
+  cached.conn = await cached.promise;
 
-}; // Add this closing curly brace
-
+  return cached.conn;
+}
