281553c4649500f32c1103c3c8feedf101f85b31
gabrielserrao <39321291+gabrielserrao@users.noreply.github.com>
2024-03-04 19:49:38 +0100

    try to fix

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -22,7 +22,7 @@ export const connectToDatabase = async () => {
     if(!MONGODB_URL) throw new Error("MONGO_URL is not defined");
 
     cached.promise = cached.promise || mongoose.connect(MONGODB_URL, 
-        {dbName:"cluster0" , bufferCommands: false })
+        {dbName:"Cluster0" , bufferCommands: false })
 
     cached.conn = await cached.promise;
     return cached.conn;
